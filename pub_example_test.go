1e070aae9a33a0077aa2c459691a973f967f50cb
Mayur Das <mayur.das1@tolexo.com>
2018-06-20 17:51:57 +0530

    Update pub_example_test.go

--- pub_example_test.go
+++ pub_example_test.go
@@ -1,18 +1,15 @@
 package drift
 
 import (
-	"flag"
 	"fmt"
-
 	"github.com/mayur-tolexo/drift"
 )
 
 // new pub created to publish message to nsqd
 func ExampleNewPub() {
-	msg := flag.String("msg", "Hi this is a test", "Message to broadcast")
-	flag.Parse()
 	d := drift.NewPub("127.0.0.1:4151")
-	if resp, err := d.Publish("elastic", *msg); err == nil {
+	msg := "This is a test"
+	if resp, err := d.Publish("elastic", msg); err == nil {
 		fmt.Println(resp)
 	} else {
 		fmt.Println(err.Error())
