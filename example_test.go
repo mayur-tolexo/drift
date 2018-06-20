5c2a5254c9e669cfdd2c502605423552c291bf4c
MaYurD <md@pooraa.com>
2018-06-20 17:42:23 +0530

    examples added

--- example_test.go
+++ example_test.go
@@ -21,12 +21,18 @@ func printIT3(value ...interface{}) error {
 	return nil
 }
 
-func Example() {
+// New consumer created with handel to call by the consumer.
+// This will start new server to receive request over HTTP
+func ExampleNewConsumer() {
 	//Default handler is printIT
 	d := drift.NewConsumer(printIT)
+
 	//elastic v6.2 handler is printIT2
 	d.AddChanelHandler("elastic", "v6.2", printIT2)
+
 	//elastic all channels handler, except v6.2, is printIT3
 	d.AddTopicHandler("elastic", printIT3)
+
+	//port assign here is 1500
 	d.Start(1500)
 }
