b12907f3f1eb7e5db4438151081c0e6c08450cb7
MaYurD <md@pooraa.com>
2018-06-20 17:26:19 +0530

    drift example added

---
+++ example_test.go
@@ -0,0 +1,32 @@
+package drift
+
+import (
+	"fmt"
+
+	"github.com/mayur-tolexo/drift"
+)
+
+func printIT(value ...interface{}) error {
+	fmt.Println("In 1st Print", value)
+	return nil
+}
+
+func printIT2(value ...interface{}) error {
+	fmt.Println("In 2nd Print", value)
+	return nil
+}
+
+func printIT3(value ...interface{}) error {
+	fmt.Println("In 3rd Print", value)
+	return nil
+}
+
+func Example() {
+	//Default handler is printIT
+	d := drift.NewConsumer(printIT)
+	//elastic v6.2 handler is printIT2
+	d.AddChanelHandler("elastic", "v6.2", printIT2)
+	//elastic all channels handler, except v6.2, is printIT3
+	d.AddTopicHandler("elastic", printIT3)
+	d.Start(1500)
+}
