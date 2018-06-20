faf765402e8b75e7aaf24df0f4c3551b799ed6df
MaYurD <md@pooraa.com>
2018-06-20 18:07:21 +0530

    master: all example added

--- pub_example_test.go
+++ pub_example_test.go
@@ -18,3 +18,27 @@ func ExampleNewPub() {
 		fmt.Println(err.Error())
 	}
 }
+
+// This will map a new handeler with specified topic's channel
+func ExampleAddChanelHandler() {
+	d := drift.NewConsumer(nil)
+	topic := "elastic"
+	channel := "v6.2"
+	d.AddChanelHandler(topic, channel, printIT)
+}
+
+// This will map a new handeler with all channels of the specified topic.
+// If a channelHandler is already mapped with any channel of the specified topic then that handler will be called
+// and in rest of the channel this handler will be called.
+func ExampleAddTopicHandler() {
+	d := drift.NewConsumer(nil)
+	topic := "elastic"
+	d.AddChanelHandler(topic, printIT)
+}
+
+// This will start the drift server to receive request over HTTP
+func ExampleStart() {
+	d := drift.NewConsumer(printIT)
+	port := 1500
+	d.Start(port)
+}
